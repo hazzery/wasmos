4e9795deb7425df1cdc21be85721f59d6b0c62d0
Harry Parkes <harrydparkes@proton.me>
2025-03-06 13:21:01 +1300

    Feature: Passing string from user input to wasm code to parse and evaluate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import * as React from 'react';
+import { TextField } from '@mui/material'
+import { compute, Coordinate } from "../public/wasm/wasmos.js";
+
 import './App.css'
 
 function App() {
+  const [coords, setCoords] = React.useState<Coordinate[]>([]);
+
+  function graph(expression: string) {
+    console.log(`Asked to compute ${expression}`)
+    let coords = compute(expression);
+    setCoords(coords);
+
+    for (let coord of coords) {
+      console.log(`(${coord.x}, ${coord.y})`);
+    }
+  }
+
   return (
     <>
       <h1>Wasmos</h1>
+      <TextField
+        id="text-field"
+        label="Input expression"
+        variant="outlined"
+        onChange={(event) => graph(event.target.value)}
+      />
     </>
   )
 }
