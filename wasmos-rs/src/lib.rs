545d33b189a5c49f0bc7334c36750b1ad8a95a8e
Harry Parkes <harrydparkes@proton.me>
2025-03-06 13:10:40 +1300

    Feature: pasted in evalexpr rust code

--- wasmos-rs/src/lib.rs
+++ wasmos-rs/src/lib.rs
@@ -1,14 +1,54 @@
 mod utils;
 
+use evalexpr::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+pub struct Coordinate {
+    x: f64,
+    y: f64,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    log("Hello, wasmos!");
+impl Coordinate {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f64, y: f64) -> Coordinate {
+        Coordinate { x, y }
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+fn do_the_math(equation: &str) -> Result<Vec<Coordinate>, EvalexprError> {
+    let operator_tree = evalexpr::build_operator_tree(equation)?;
+
+    let mut the_math: Vec<Coordinate> = Vec::new();
+    for x in 0..100 {
+        let context = context_map! { "x" => int x }?;
+        let y = operator_tree.eval_float_with_context(&context)?;
+        the_math.push(Coordinate { x: x as f64, y });
+    }
+
+    Ok(the_math)
+}
+
+#[wasm_bindgen]
+pub fn compute(equation: &str) -> Vec<Coordinate> {
+    match do_the_math(equation) {
+        Ok(points) => points,
+        Err(error) => panic!("{}", error),
+    }
 }
