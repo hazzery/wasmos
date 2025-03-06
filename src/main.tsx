f7bfd5257bb6f524bd93df31caca0a7d301bd386
Harry Parkes <harrydparkes@proton.me>
2025-03-06 13:08:39 +1300

    Chore: More setup of JS things

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import init from "../public/wasm/wasmos.js";
+
+await init();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
