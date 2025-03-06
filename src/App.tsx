4a30c572a8d3ecf36c09e679ded285b99d82c955
Harry Parkes <harrydparkes@proton.me>
2025-03-06 13:26:55 +1300

    Feature: Attempt at using function-plot

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,27 @@ import * as React from 'react';
 import { TextField } from '@mui/material'
 import { compute, Coordinate } from "../public/wasm/wasmos.js";
 
+import functionPlot from "function-plot";
+
 import './App.css'
+
+let contentsBounds = document.body.getBoundingClientRect();
+let width = 800;
+let height = 500;
+let ratio = contentsBounds.width / width;
+width *= ratio;
+height *= ratio;
+
+functionPlot({
+  target: "#root",
+  width,
+  height,
+  yAxis: { domain: [-1, 9] },
+  grid: true,
+  data: [
+    { fn: "x^2" }
+  ]
+});
 
 function App() {
   const [coords, setCoords] = React.useState<Coordinate[]>([]);
