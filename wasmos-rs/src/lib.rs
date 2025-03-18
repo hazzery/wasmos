50bbf24e77cc3259ed5d6b84446c483cc49b84b8
Harrison Parkes <hpa101@uclive.ac.nz>
2025-03-18 14:20:45 +1300

    Documentation: Added inline comments to rust code

--- wasmos-rs/src/lib.rs
+++ wasmos-rs/src/lib.rs
@@ -18,8 +18,10 @@ fn do_the_math(equation: &str) -> Result<Vec<Coordinate>, evalexpr::EvalexprErro
 
     let mut the_math: Vec<Coordinate> = Vec::new();
 
+    // We are not actually truncating anything here as WASM is 32-bit.
     #[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
     let upper_limit = GRAPH_RESOLUTION as i32 / 2;
+
     for x in -upper_limit..upper_limit {
         let context = evalexpr::context_map! { "x" => int x }?;
         let y = operator_tree.eval_number_with_context(&context)?;
@@ -62,7 +64,7 @@ fn main() {
     /* Create the ground. */
     collider_set.insert(ColliderBuilder::cuboid(100.0, 0.1).build());
 
-    /* Create the bouncing ball. */
+    /* Create the bouncing ball, setting initial position. */
     let rigid_body = RigidBodyBuilder::dynamic()
         .translation(nalgebra::vector![0.0, 10.0])
         .build();
