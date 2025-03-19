20a8d06d58ad423109b5188012c3865766c038c3
Harrison Parkes <hpa101@uclive.ac.nz>
2025-03-19 21:07:45 +1300

    Feature: I Cooked, graphs now working very nicely in the ChartContainer

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,8 +1,9 @@
 import Paper from '@mui/material/Paper';
+import { LinePlot } from '@mui/x-charts';
 import { ChartContainer } from '@mui/x-charts/ChartContainer';
-import { LineChart } from '@mui/x-charts/LineChart';
-import { ScatterPlot } from '@mui/x-charts';
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
 import { LineSeriesType } from '@mui/x-charts/models/seriesType/line';
 import * as React from 'react';
 
@@ -11,6 +12,10 @@ import InputBar from '../components/InputBar.js';
 
 function Graph() {
   const [series, setSeries] = React.useState<LineSeriesType[]>([]);
+
+  function range(start: number, end: number) {
+    return Array.from(Array(end - start + 1).keys()).map(x => x + start);
+  }
 
   function graph(expression: string, index: number) {
     let coordinates = expression !== "" ? compute(expression) : [];
@@ -33,33 +38,18 @@ function Graph() {
         onChangeCallback={(event, index) => graph(event.target.value, index)}
       ></InputBar>
       <Paper>
-        <LineChart
-          series={series}
-          // xAxis={[{ label: "x", min: -50, max: 50, data: Array(100) }]}
-          // yAxis={[{ label: "y", min: 0, max: 100 }]}
-          grid={{ vertical: true, horizontal: true }}
-          sx={{
-            [`.${axisClasses.left} .${axisClasses.label}`]: {
-              transform: 'translate(-10px, 0)',
-            },
-          }}
-          width={900}
-          height={600}
-        />
         <ChartContainer
           series={series}
-          width={900}
-          height={600}
-          // xAxis={[{ label: "x", min: -50, max: 50, data: Array(100) }]}
-          // yAxis={[{ label: "y", min: 0, max: 100 }]}
-          // grid={{ vertical: true, horizontal: true }}
-          // sx={{
-          //   [`.${axisClasses.left} .${axisClasses.label}`]: {
-          //     transform: 'translate(-10px, 0)',
-          //   },
-          // }}
+          width={1649}
+          height={900}
+          xAxis={[{ label: "x", min: -50, max: 50, data: range(-50, 50) }]}
+          yAxis={[{ label: "y", min: -50, max: 50 }]}
         >
-          <ScatterPlot />
+          <ChartsXAxis />
+          <ChartsYAxis />
+          <ChartsReferenceLine x={0} lineStyle={{ strokeDasharray: '10 5' }} />
+          <ChartsReferenceLine y={0} lineStyle={{ strokeDasharray: '10 5' }} />
+          <LinePlot />
         </ChartContainer>
       </Paper >
     </>
