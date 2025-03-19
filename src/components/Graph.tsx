24cd2a6ba998aace6ffc1326f9e74285c27515a4
Harrison Parkes <hpa101@uclive.ac.nz>
2025-03-19 20:29:42 +1300

    I had a bad idea

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,63 +1,53 @@
 import Paper from '@mui/material/Paper';
-import { ChartContainer } from '@mui/x-charts/ChartContainer';
-import { LineChart } from '@mui/x-charts/LineChart';
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
-import { LineSeriesType } from '@mui/x-charts/models/seriesType/line';
+import { Mafs, Coordinates, Point } from "mafs";
 import * as React from 'react';
 
-import { compute } from "../../public/wasm/wasmos.js";
+import { compute, Coordinate } from "../../public/wasm/wasmos.js";
 import InputBar from '../components/InputBar.js';
 
 function Graph() {
-  const [series, setSeries] = React.useState<LineSeriesType[]>([]);
+  let [coords, setCoords] = React.useState<Coordinate[][]>([]);
 
   function graph(expression: string, index: number) {
     let coordinates = expression !== "" ? compute(expression) : [];
 
-    setSeries(previousSeries => {
-      let newSeries = [...previousSeries];
-      newSeries[index] = {
-        type: 'line',
-        label: `Series ${series.length}`,
-        data: coordinates.map((coordinate) => coordinate.y),
-        showMark: false,
-      };
-
-      return newSeries;
+    setCoords(previousCoords => {
+      let newCoords = [...previousCoords];
+      newCoords[index] = coordinates;
+      return newCoords;
     });
   }
+
+  function points(coordinates: Coordinate[]) {
+    return coordinates.map((coord: Coordinate, index: number) =>
+      <Point
+        key={index}
+        x={coord.x}
+        y={coord.y}
+      />
+    );
+  }
+
+  function graphs() {
+    return coords.map((coordinates: Coordinate[]) => points(coordinates));
+  }
+
   return (
     <>
       <InputBar
         onChangeCallback={(event, index) => graph(event.target.value, index)}
       ></InputBar>
       <Paper>
-        <LineChart
-          series={series}
-          // xAxis={[{ label: "x", min: -50, max: 50, data: Array(100) }]}
-          // yAxis={[{ label: "y", min: 0, max: 100 }]}
-          grid={{ vertical: true, horizontal: true }}
-          sx={{
-            [`.${axisClasses.left} .${axisClasses.label}`]: {
-              transform: 'translate(-10px, 0)',
-            },
-          }}
+
+        <Mafs
+          zoom={{ min: 0.000001, max: Infinity }}
           width={900}
           height={600}
-        />
-        <ChartContainer
-          series={series}
-          width={900}
-          height={600}
-          // xAxis={[{ label: "x", min: -50, max: 50, data: Array(100) }]}
-          // yAxis={[{ label: "y", min: 0, max: 100 }]}
-          // grid={{ vertical: true, horizontal: true }}
-        // sx={{
-        //   [`.${axisClasses.left} .${axisClasses.label}`]: {
-        //     transform: 'translate(-10px, 0)',
-        //   },
-        // }}
-        />
+        >
+          <Coordinates.Cartesian
+            xAxis={{ lines: 1, subdivisions: false }} />
+          {coords.length > 0 ? graphs() : "No points"}
+        </Mafs>
       </Paper>
     </>
   );
